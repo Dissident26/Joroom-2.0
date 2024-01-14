82aea7d69c17b28a0379b92b200a68210c4999a2
Dissident26 <blackhate@bk.ru>
2024-01-14 17:03:58 +0300

    add routes

---
+++ backend/src/routes/routes.ts
@@ -0,0 +1,6 @@
+export const routes = {
+  users: {
+    root: '/users',
+    user: '/users/:id',
+  },
+};
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,11 +1,20 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Param } from '@nestjs/common';
+
 import { UserService } from './user.service';
+import { routes } from '../routes/routes';
+import { User } from '../entities/user.entity';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Get('/users')
-  root() {
+
+  @Get(routes.users.root)
+  findAll(): Promise<User[]> {
     return this.userService.findAll();
+  }
+
+  @Get(routes.users.user)
+  findOne(@Param('id') id: string): Promise<User | null> {
+    return this.userService.findOne(parseInt(id));
   }
 }
