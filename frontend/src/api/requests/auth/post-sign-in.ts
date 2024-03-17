61cba11fc3e962301161f3eb2ce00b9ef5db8ab9
Dissident26 <blackhate@bk.ru>
2024-03-17 13:35:27 +0300

    FE: withCredentials false to signIn

--- frontend/src/api/requests/auth/post-sign-in.ts
+++ frontend/src/api/requests/auth/post-sign-in.ts
@@ -2,7 +2,9 @@ import { api } from '@/api';
 import { SignInDto } from '@/types';
 
 export const postSignIn = async (signInData: SignInDto) => {
-  const { data } = await api.auth.signIn(signInData);
+  const { data } = await api.auth.signIn(signInData, {
+    withCredentials: false,
+  });
 
   return data;
 };
