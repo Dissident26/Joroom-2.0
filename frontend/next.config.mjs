ad5fd655fcf52b67daa1b46cd1d95beba240b7b5
Artem Zhukovsky <azhukovsky@enterrasolutions.com>
2024-03-27 16:13:54 +0100

    FE: add logging

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
     BASE_API_URL: process.env.BASE_API_URL,
     PORT: process.env.PORT,
   },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
